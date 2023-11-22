c77b107c19c991bc3bc3a93a43af85122fd2347a
doudou <agilebeanisartist@gmail.com>
2023-11-23 03:47:17 +0800

    complete python today practice

---
+++ practice/ch5/conditional_tests.py
@@ -0,0 +1,12 @@
+number = 5 
+print("Is number > '1'? I predict True.")
+print(5 > 1)
+
+print("\nIs number > '2'? I predict True.")
+print(5 > 2)
+
+print("\nIs number > '8'? I predict False.")
+print(5 > 8)
+
+print("\nIs number > '10'? I predict False.")
+print(5 > 10)
---
+++ practice/ch5/more_conditional_tests.py
@@ -0,0 +1,18 @@
+name = "Jinli"
+
+print(name == 'jinlo')
+print(name.lower() == 'jinli')
+
+number = 10
+print(number == 10)
+print(number != 10)
+print(number > 5)
+print(number < 5)
+print(number >= 5)
+print(number <= 5)
+print(number > 5 and number > 10)
+print(number > 5 or number < 20)
+
+list = [1,2,3,4]
+print(1 in list)
+print(5 not in list)
