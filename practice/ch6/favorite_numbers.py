c1fe00ebed006f3415c68994e7fbcba22ede5f6f
lin <linhuang@yeahka.com>
2023-11-29 14:50:52 +0800

    compleate per day practice

---
+++ practice/ch6/favorite_numbers.py
@@ -0,0 +1,3 @@
+persons = {"Jack": 1, "John": 15, "Maria": 88, "Gragon": 66, "Bradon": 123}
+for name, number in persons.items():
+    print(f"{name}'s favorite number is {number}")
---
+++ practice/ch6/person.py
@@ -0,0 +1,9 @@
+person = {
+    "first_name": "james",
+    "last_name": "harden",
+    "age": 32,
+    "city": "Los Angeles",
+}
+
+for key, value in person.items():
+    print(f"the {key} is {value}")
