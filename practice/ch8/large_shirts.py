f77d128497c180186b132cd6f104c57e7afbc251
lin <linhuang@yeahka.com>
2023-12-07 16:07:36 +0800

    complete perday practice

---
+++ practice/ch8/large_shirts.py
@@ -0,0 +1,7 @@
+def make_shirt(size="large", message="I love Python"):
+    print(f"the t-shirt's size is {size} and message is {message}")
+
+
+make_shirt()
+make_shirt("medium")
+make_shirt("small", "hello py")
---
+++ practice/ch8/t-shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    print(f"the t-shirt's size is {size} and message is {message}")
+
+
+make_shirt(20, "hello-python")
+make_shirt(size=20, message="hello,python")
